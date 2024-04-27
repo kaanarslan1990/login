b6298010091f40f008127911a956a4e67b4bde4f
okarslan <okarslan@dabnet.com.tr>
2024-04-27 12:54:00 +0300

    login-page design

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,18 +1,25 @@
-import { KeyboardAvoidingView, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
+import {
+  KeyboardAvoidingView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import React from "react";
 
 export default function LoginScreen() {
   return (
     <KeyboardAvoidingView style={styles.container} behavior="padding">
       <View style={styles.inputContainer}>
-        <TextInput style={styles.input} placeholder="Email"/>
+        <TextInput style={styles.input} placeholder="Email" />
         <TextInput style={styles.input} placeholder="Password" />
       </View>
       <View style={styles.buttonContainer}>
         <TouchableOpacity style={styles.button}>
           <Text style={styles.buttonText}>Sign In</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={[styles.buttonContainer,styles.outlineButton]}>
+        <TouchableOpacity style={[styles.button, styles.outlineButton]}>
           <Text style={styles.outlineButtonText}>Sign Up</Text>
         </TouchableOpacity>
       </View>
@@ -21,14 +28,43 @@ export default function LoginScreen() {
 }
 
 const styles = StyleSheet.create({
-  container:{},
-  inputContainer:{},
-  input:{},
-  buttonContainer:{},
-  button:{},
-  buttonText:{},
-  outlineButton:{},
-  outlineButtonText:{}
-
-
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  inputContainer: {
+    width: "80%",
+  },
+  input: {
+    backgroundColor: "white",
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    marginTop: 5,
+    borderRadius: 10,
+  },
+  buttonContainer: {
+    width: "60%",
+    marginTop: 40,
+  },
+  button: {
+    backgroundColor: "#0782F9",
+    padding: 15,
+    alignItems: "center",
+    borderRadius: 10,
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 16,
+    fontWeight:'700'
+  },
+  outlineButton: {
+    backgroundColor: "white",
+    marginTop: 5,
+  },
+  outlineButtonText: {
+    color: "#0782F9",
+    fontSize: 16,
+    fontWeight:'700'
+  },
 });
